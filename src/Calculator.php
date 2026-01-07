4b6500e60722dfd30e56aab3fafd8162004247d0
tharathonp-debug <tharathon.p@rmutsvmail.com>
2026-01-07 16:52:14 +0700

    Create Calculator.php

---
+++ src/Calculator.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+class Calculator
+{
+  public function add(int $a, int $b): int
+{
+    return $a + $b;
+}
+  public function subtract(int $a, int $b): int
+{
+    return $a - $b;
+}
+}
